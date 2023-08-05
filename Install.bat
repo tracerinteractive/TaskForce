260a630aab92da099ffd24c16e0fa23c5cd97860
Tracer Interactive <43829991+tracerinteractive@users.noreply.github.com>
2023-08-05 10:31:44 -0500

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,11 +1,9 @@
 @echo off
-git clone -b taskforce https://github.com/tracerinteractive/UnrealEngine.git
+set /P "PAC=GitHub Personal Access Token: "
+git clone -b taskforce https://%PAC%@github.com/tracerinteractive/UnrealEngine.git
 
 if exist UnrealEngine\Setup.bat (
 	move UnrealEngine UnrealEngine-taskforce
 )
 
 pause
-if exist UnrealEngine-taskforce\Setup.bat (
-	del "%~f0" & exit
-)
