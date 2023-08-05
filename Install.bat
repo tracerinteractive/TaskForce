eabb154f82a6cbf67d98128e176d0d97fe208a82
Tracer Interactive <43829991+tracerinteractive@users.noreply.github.com>
2023-08-05 11:13:31 -0500

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,4 +1,11 @@
 @echo off
+
+where git >nul 2>nul
+if %ERRORLEVEL% neq 0 (
+	echo ERROR: Install git!
+	goto :error
+)
+
 set /P "PAC=GitHub Personal Access Token: "
 git clone -b taskforce https://%PAC%@github.com/tracerinteractive/UnrealEngine.git
 
